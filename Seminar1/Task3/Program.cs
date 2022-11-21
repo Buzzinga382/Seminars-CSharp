c0a6c0d99b490a39bac4bcdbefcad049305d6588
Anthony <gugl382@gmail.com>
2022-11-21 21:27:04 +0300

    Task4 completed

--- Seminar1/Task3/Program.cs
+++ Seminar1/Task3/Program.cs
@@ -4,31 +4,36 @@ Console.WriteLine("Enter number");
 
 int number = int.Parse(Console.ReadLine());
 
-if(number == 1)
+if (number < 1 | number > 7)
+{
+    Console.WriteLine("Error");
+}
+
+if (number == 1)
 {
     Console.WriteLine("Monday");
 }
-if(number == 2)
+if (number == 2)
 {
     Console.WriteLine("Tuesday");
 }
-if(number == 3)
+if (number == 3)
 {
     Console.WriteLine("Wendsday");
 }
-if(number == 4)
+if (number == 4)
 {
     Console.WriteLine("Thursday");
 }
-if(number == 5)
+if (number == 5)
 {
     Console.WriteLine("Friday");
 }
-if(number == 6)
+if (number == 6)
 {
     Console.WriteLine("Saturday");
 }
-if(number == 7)
+if (number == 7)
 {
     Console.WriteLine("Sunday");
 }
---
+++ Seminar1/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4 (Ввести целое число N, и показать все целые числа от -N до N)
+
+Console.WriteLine("Enter N");
+
+int number = int.Parse(Console.ReadLine());
+
+int count = number * (-1);
+
+while (count <= number)
+{
+    Console.WriteLine(count);
+    count = count + 1;
+}
