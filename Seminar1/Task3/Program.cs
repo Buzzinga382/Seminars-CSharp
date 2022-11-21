c077785b8b7a4a7e7a6bcd7d603c38f746ff2667
Anthony <gugl382@gmail.com>
2022-11-21 20:54:58 +0300

    Task3 completed

--- Seminar1/Task3/Program.cs
+++ Seminar1/Task3/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3 (Нужно вывести день недели по введенному числу)
+
+Console.WriteLine("Enter number");
+
+int number = int.Parse(Console.ReadLine());
+
+if(number == 1)
+{
+    Console.WriteLine("Monday");
+}
+if(number == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if(number == 3)
+{
+    Console.WriteLine("Wendsday");
+}
+if(number == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if(number == 5)
+{
+    Console.WriteLine("Friday");
+}
+if(number == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if(number == 7)
+{
+    Console.WriteLine("Sunday");
+}
