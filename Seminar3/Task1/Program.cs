19769e5659c0d15ab5beb9bc0d064b942586ee96
Anthony <gugl382@gmail.com>
2022-11-28 20:27:05 +0300

    Task1 completed

--- Seminar3/Task1/Program.cs
+++ Seminar3/Task1/Program.cs
@@ -1,3 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Программа, которая принимает на вход Х и У, причём они не ноль,
+// и выдаёт номер четверти плоскости, в которой находится эта точка
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите координату X");
+
+bool isParsedX = int.TryParse(Console.ReadLine(), out int x);
+
+Console.WriteLine("Введите координату Y");
+
+bool isParsedY = int.TryParse(Console.ReadLine(), out int y);
+
+if(!isParsedX || !isParsedY || x == 0 || y == 0) // По идее отсюда можно удалить проверку равенства координат нулю, потому что мы потом проверили это в методе с помощью -1
+{
+    Console.WriteLine("Неверные координаты");
+    return;
+} 
+
+int quarterNumber = GetQuarterNumberByCoordinates(x, y);
+
+if(quarterNumber == -1)
+{
+    Console.WriteLine("Координаты равны нулю");
+    return;
+}
+
+// возвращаемый_тип_данных НазваниеМетода()
+// { 
+//    тело метода
+//    оператор return
+// }
+
+Console.WriteLine($"Номер четверти {quarterNumber}");
+
+int GetQuarterNumberByCoordinates(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+
+    if(x < 0 && y < 0)
+    {
+        return 3;
+    }
+    
+    if(x > 0 && y < 0)
+    {
+        return 4;
+    }
+
+    return -1;
+
+}
