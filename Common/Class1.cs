21ba801ed28020a4c44dc2df8a6f530d72aa3583
Anthony <gugl382@gmail.com>
2022-12-12 20:16:25 +0300

    Common modified

--- Common/Class1.cs
+++ Common/Class1.cs
@@ -30,4 +30,31 @@ public static class Helper
         }
         return reverseArray;
     }
+
+    public static void Print2DArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static int[,] CreateRandom2DArray(int countOfRows, int countOfColumns)
+    {
+        Random random = new Random();
+        int[,] array = new int[countOfRows, countOfColumns];
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = random.Next(-10, 11);
+            }
+        }
+        return array;
+    }
 }
